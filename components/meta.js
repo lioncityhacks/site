becd8e2c2bdf2ad4ef80ef5ee3deba061bcd006c
Sam Poder <39828164+sampoder@users.noreply.github.com>
2022-08-31 07:49:30 +0800

    Roar!

--- components/meta.js
+++ components/meta.js
@@ -5,9 +5,9 @@ const makeTitle = (title, name) =>
   title === name ? title : `${title} – ${name}`
 
 const Meta = ({
-  title = '🦁 Lion City Hacks', // page title
-  name = '🦁 Lion City Hacks', // site name
-  description = "We're inviting you to 🦁 Lion City Hacks, a hackathon for teenagers in Singapore, on December 3rd at Google Singapore. Join us for twelve hours of hacking, workshops & friendship.", // page description
+  title = 'Lion City Hacks', // page title
+  name = 'Lion City Hacks', // site name
+  description = "🦁 We're inviting you to Lion City Hacks, a hackathon for teenagers in Singapore, on December 3rd at Google Singapore. Join us for twelve hours of hacking, workshops & friendship.", // page description
   image = 'https://cloud-bbc4b3xr0-hack-club-bot.vercel.app/0twitter_post_-_1-2.png', // social card image URL
   url = 'https://next-theme-starter.vercel.app',
   children,
