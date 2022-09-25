e68368220c3d6b047f80e80d65c3e3ad06d93636
Sam Poder <sam.r.poder@gmail.com>
2022-09-25 17:37:18 +0800

    Update meta.js

--- components/meta.js
+++ components/meta.js
@@ -7,7 +7,7 @@ const makeTitle = (title, name) =>
 const Meta = ({
   title = 'Lion City Hacks', // page title
   name = 'Lion City Hacks', // site name
-  description = "We're inviting you to Lion City Hacks, a hackathon for teenagers in Singapore, on December 3rd at Red Hat Singapore. Join us for twelve hours of hacking, workshops & friendship.", // page description
+  description = "We're inviting you to Lion City Hacks, a hackathon for teenagers in Singapore, on December 3rd at Red Hat Singapore. Join us for twelve hours of hacking, workshops & friendship. All secondary school, junior college & pre-university students are welcome!", // page description
   image = 'https://cloud-eapm8zekq-hack-club-bot.vercel.app/0twitter_post_-_2.png', // social card image URL
   url = 'https://lioncityhacks.com',
   children,
