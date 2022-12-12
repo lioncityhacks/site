fd90123a059c4be9502386768a6f37f1da0ff961
Sam Poder <39828164+sampoder@users.noreply.github.com>
2022-12-13 00:05:59 +0700

    Archive the meta too!

--- components/meta.js
+++ components/meta.js
@@ -7,8 +7,8 @@ const makeTitle = (title, name) =>
 const Meta = ({
   title = `Lion City Hacks - Singapore's hackathon for teenagers.`, // page title
   name =  `Lion City Hacks - Singapore's hackathon for teenagers.`, // site name
-  description = "Lion City Hacks is taking place on December 3rd at Red Hat Singapore. All secondary school, junior college & polytechnic students are welcome at the hackathon!", // page description
-  image = 'https://cloud-eapm8zekq-hack-club-bot.vercel.app/0twitter_post_-_2.png', // social card image URL
+  description = "150 teengers gathered in Singapore at Red Hat's APAC HQ on December 3rd 2022 to build apps, websites, games and more!", // page description
+  image = 'https://cloud-ktiz3dyto-hack-club-bot.vercel.app/0twitter_post_-_3.png', // social card image URL
   url = 'https://lioncityhacks.com',
   target,
   children,
